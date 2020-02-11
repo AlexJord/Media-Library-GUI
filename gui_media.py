4986c3fbde28bf31046ec7bd22b3e708b410cded
AlexJord <ajordan@stcharlessd.org>
2020-02-11 12:56:49 -0600

    modified:   gui_media.py

--- gui_media.py
+++ gui_media.py
@@ -17,7 +17,39 @@ class MainMenu(tk.Frame):
         self.lbl_title = tk.Label(text = "Game Library", font = TITLE_FONT)
         self.lbl_title.grid(row = 0, column = 0, sticky = "news")
         self.btn_add = tk.Button(text = "Add", font = BUTTON_FONT)
-        self.btn_add.grid(row = 1, column = 0)
+        self.btn_add.grid(row = 1, column = 0)       
+        
+class SearchMenu(tk.Frame):
+    def __init__(self):
+        tk.Frame.__init__(self)
+        self.lbl_title = tk.Label(text = "Search", font = TITLE_FONT)
+        self.lbl_title.grid(row = 0, column = 0, sticky = "news")
+        
+        self.lbl_search = tk.Label(text = "Search by: ", font = BUTTON_FONT)
+        self.lbl_search.grid(row = 1, column = 0, sticky = "news")
+        
+        self.lbl_search2 = tk.Label(text = "Search for: ", font = BUTTON_FONT)
+        self.lbl_search2.grid(row = 3, column = 0, sticky = "news")        
+        
+        self.ent_box1 = tk.Entry()
+        self.ent_box1.grid(row = 2, column = 0)
+        
+        self.ent_box2 = tk.Entry()
+        self.ent_box2.grid(row = 4, column = 0)
+        
+        
+        
+        self.scrolled_text = ScrolledText()
+        self.scrolled_text.grid(row = 5, column = 0, columnspan = 3)        
+        
+        
+            
+         
+        
+         
+        
+        
+        
 
 ##MAIN
 if __name__ == "__main__":
@@ -27,8 +59,15 @@ if __name__ == "__main__":
     root = tk.Tk()
     root.title("Game Lib")
     root.geometry("500x500")
+    
     main_menu = MainMenu()
     main_menu.grid(row = 0, column = 0)
+    
+    search_menu = SearchMenu()
+    search_menu.grid(row = 0, column = 0)
+    
+    root.grid_rowconfigure(0, weight = 1)
+    root.grid_columnconfigure(0, weight = 1)
     
     root.mainloop()
     
